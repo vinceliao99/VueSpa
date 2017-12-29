badb9d78704100d2d1abc6a64ffbd2a669386ff2
Vincent Liao <vincentliao@kingstono365.onmicrosoft.com>
2017-12-29 17:22:27 +0800

    Add new component

--- ClientApp/components/app-root.vue
+++ ClientApp/components/app-root.vue
@@ -19,11 +19,13 @@ import CounterExample from './counter-example'
 import FetchData from './fetch-data'
 import HomePage from './home-page'
 import NavMenu from './nav-menu'
+import Test from './test'
 
 Vue.component('counter-example', CounterExample);
 Vue.component('fetch-data', FetchData);
 Vue.component('home-page', HomePage);
 Vue.component('nav-menu', NavMenu);
+Vue.component('test', Test);
 
 export default {
     data() {
---
+++ ClientApp/components/test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Counter</h1>
+
+        <p>This is a simple example of a Vue.js component & Vuex</p>
+
+        <p>
+            Current count (Vuex): <strong>{{ currentCount }}</strong>
+        </p>
+        <p>
+            Auto count: <strong>{{ autoCount }}</strong>
+        </p>
+
+        <button @click="incrementCounter()">Increment</button>
+        <button @click="resetCounter()">Reset</button>
+    </div>
+</template>
+<script src="../components/testTemplate.js"></script>
